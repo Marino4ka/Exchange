b6ca26a7a286860e61388bbe0205bcb9ebc864c4
Marino4ka <marina.iovbak@gmail.com>
2016-09-25 12:48:38 +0600

    second version

--- leap year baby.py
+++ leap year baby.py
@@ -1,18 +1,20 @@
 def is_leap_baby(day,month,year):
-   if day == 29 and month == 2 and year%100 != 0 and year%400 == 0 or year%4 == 0:
-   		status = True
-   else:
-   		status = False
-   		return status
+	if day == 29 and month == 2 and year%100 != 0 and year%400 == 0 or year%4 == 0:
+		status = True
+	else:
+		status = False
+	print (status)
 
 
-# The function 'output' prints one of two statements based on whether 
-# the is_leap_baby function returned True or False.
+def output(status,name):
+	if status == True:
+		print ("%s is one of an extremely rare species. He is a leap year baby!" % name)
+	else:
+		print ("There's nothing special about %s's birthday. He is not a leap year baby!" % name)
 
-#def output(status,name):
-    #if status:
-        #print "%s is one of an extremely rare species. He is a leap year baby!" % name
-    #else:
-        #print "There's nothing special about %s's birthday. He is not a leap year baby!" % name
 
-is_leap_baby(day,month,year)
+output(is_leap_baby(29, 2, 1900), 'Charlie Brown')
+#>>>There's nothing special about Charlie Brown's birthday. He is not a leap year baby!
+
+output(is_leap_baby(28, 2, 1976), 'Odie')
+
